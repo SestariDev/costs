247058011762f0775cbf08ec79c9da08cae4c92b
Sestari <Guilherme.Sestari@br.nestle.com>
2024-07-15 19:17:23 -0300

    test

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import React, { useState } from 'react';
 import Contact from './components/pages/Contact';
 import Home from './components/pages/Home';
-import Work from './components/pages/Work';
-import NavBar from './components/nav/NavBar';
-import Footer from './components/nav/Footer';
-import NewProject from './components/pages/NewProject';
 import WheelOfFortuneGame from './components/pages/Projects';
 import Perfil from './components/pages/Perfil';
-
+import NavBar from './components/nav/NavBar';
+import Footer from './components/nav/Footer';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('contact');
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'home':
+        return <Home />;
+      case 'wheelOfFortuneGame':
+        return <WheelOfFortuneGame />;
+      case 'perfil':
+        return <Perfil />;
+      default:
+        return <Contact setCurrentPage={setCurrentPage} />;
+    }
+  };
+
   return (
     <div className="App">
-
-      <Router>
-        <NavBar/>
-        <Routes>
-          <Route path="/" element={<Contact />} />
-          <Route path="/pic" element={<Home />} />
-          <Route path="/roda" element={<WheelOfFortuneGame />} />
-          <Route path="/perfil" element={<Perfil />} /> 
-        </Routes>
-        <Footer/>
-      </Router>
-      
+      <NavBar />
+      {renderPage()}
+      <Footer />
     </div>
   );
 }
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -5,19 +5,28 @@ import Locaweb from "../../img/locaweb-logo-0.png";
 import Roda from "../../img/roda.png";
 import img from '../../img/costs_logo.png';
 
-function Contact(props) {
+function Contact({ setCurrentPage }) {
   return (
     <Container>
       <div className='flex flex-wrap w-full h-full gap-4'>
-        <a href="/pic" className='rounded-lg w-[47%] flex aspect-square justify-center items-center border border-solid border-gray-500'>
+        <button 
+          onClick={() => setCurrentPage('home')} 
+          className='rounded-lg w-[47%] flex aspect-square justify-center items-center border border-solid border-gray-500'
+        >
           <img src={img} alt="Costs Logo"/>
-        </a>
-        <a href="/perfil" className='rounded-lg w-[47%] flex aspect-square justify-center items-center border '>
-          <img src={perfil} alt="serro"/>
-        </a>
-        <a href="/roda" className='rounded-lg w-[47%] flex aspect-square justify-center items-center border border-solid border-gray-500'>
-          <img src={Roda} alt="erro"/>
-        </a>
+        </button>
+        <button 
+          onClick={() => setCurrentPage('perfil')} 
+          className='rounded-lg w-[47%] flex aspect-square justify-center items-center border '
+        >
+          <img src={perfil} alt="Perfil"/>
+        </button>
+        <button 
+          onClick={() => setCurrentPage('wheelOfFortuneGame')} 
+          className='rounded-lg w-[47%] flex aspect-square justify-center items-center border border-solid border-gray-500'
+        >
+          <img src={Roda} alt="Roda"/>
+        </button>
       </div>
     </Container>
   );
